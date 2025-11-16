7e9a437797e56ad1ee82047c917e7565f187016c
Aditya Pulipaka <pulipaka24@outlook.com>
2025-11-16 12:12:37 -0600

    back to reality

--- DualYokeControl (Previous Version).ino
+++ DualYokeControl (Previous Version).ino
@@ -121,7 +121,6 @@ void loop() {
 
     dist2_raw = analogRead(indR);
     if (dist2_raw > 870) oor2 = true;
-    qoueirhpqwerpioqwejpoi;
     dist2 = ind2mm(ind1Map, dist2_raw);
     Serial.print(dist2);
     Serial.print(", ");
@@ -147,7 +146,6 @@ void loop() {
       Serial.print(", ");
       Serial.print(pwm2);
       Serial.print(", ");
-      asdfiuohasioufhaspdjifosa;
     }
     else {
       send_pwmFL(0);
